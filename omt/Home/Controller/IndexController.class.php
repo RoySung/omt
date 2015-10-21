031c5f00a0a0ecbb94cf577dc0dd8cf7dc3e9fd0
dong <aa1786482ty@gmail.com>
2015-10-21 21:18:53 +0800

    fix ticket.html IndexController

--- omt/Home/Controller/IndexController.class.php
+++ omt/Home/Controller/IndexController.class.php
@@ -40,52 +40,16 @@ class IndexController extends Controller {
         $this->display();
     }
     public function ticket(){
-        $db = M('Orderticket'); //座位,人數
+        $db = M('Orderticketview');
         $session_auth = session('auth');
         $condit['m_id'] = $session_auth['m_id'];
-        $count = $db->where($condit)->count(); //票劵數量 2
-        //$data = $db->where($condit)->field('seat,quantity')->select();
         $data = $db->where($condit)->select();
-        $session = $data['ot_id'];
 
-        for($i=0;$i<$count;$i++)
+        $this->result = 1;
+        if(!$data)
         {
-            $db = M('Movie'); //電影名稱
-            $condit['mo_id'] = $data[$i]['mo_id'];
-            $data_name = $db->where($condit)->field('movie_name')->select();
-            $data[$i]['movie_name'] = $data_name[0]['movie_name'];
-
-            $db = M('Session'); //時間,場次
-            $condit['s_id'] = $data[$i]['s_id'];
-            $data_session = $db->where($condit)->field('time,date,t_id')->select();
-            $data[$i]['time'] = $data_session[0]['time'];
-            $data[$i]['date'] = $data_session[0]['date'];
-
-            /*$db = M('Theater');//影院名稱
-            $condit['t_id'] = $data_session[0]['t_id'];
-            $data_theater = $db->where($condit)->field('theater_name')->select();
-            $data[$i]['theater_name'] = $data_theater[0]['theater_name'];*/
+            $this->result = 0;
         }
-        /*for($i=0;$i<$count;$i++)
-        {
-            $db = M('Theater');//影院名稱
-            $condit['t_id'] = $data_session[0]['t_id'];
-            $data_theater = $db->where($condit)->field('theater_name')->select();
-            $data[$i]['theater_name'] = $data_theater[0]['theater_name'];
-        }*/
-
-        /*$db = M('Movie'); //電影名稱
-        $condit['mo_id'] = $data[0]['mo_id'];
-        $data_name = $db->where($condit)->field('movie_name')->select();
-        $data[$count-2]['movie_name'] = $data_name[0]['movie_name'];
-
-        /*$db = M('Session'); //時間,場次
-        $condit['s_id'] = 1;
-        $data = $db->where($condit)->select();
-        $result[0]['time'] = $data[0]['time'];
-        $result[0]['date'] = $data[0]['date'];*/
-
-
         $this->assign('ticket',$data);
         $this->display();
     }
